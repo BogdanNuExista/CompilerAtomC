043a74d76e762f183e732480fd2576ab7fc97ee2
BogdanNuExista <bogdancraiu15@gmail.com>
2025-05-16 22:20:03 +0300

    modified test file

--- AtomC/tests/testad.c
+++ AtomC/tests/testad.c
@@ -11,20 +11,53 @@ struct S1{
 struct S1 p1;
 struct S1 vp[10];
 
+
+struct S2 {
+    struct S1 s1member;
+    int data;
+};
+
+struct S2 s2var;
+
 double sum(double x[5],int n){
 	double r;
 	int i;
 	r=0;
 	i=0;
 	while(i<n){
-		double n;
+		double r;
 		n=x[i];
 		r=r+n;
 		i=i+1;
 		}
+	{ // test pentru block nested
+		int blockVar;
+        blockVar = 100;
+        {
+            int deeperVar;
+            deeperVar = blockVar + i;
+        }
+	}
+
 	return r;
 	}
 	
 void f(struct S1 p){
 	puti(p.i);
 	}
+
+void testVoid() {
+    // Empty function
+}
+
+int testParams(int a, char b, double c, struct S1 d) {
+    return a;
+}
+
+void test(){
+	int x;
+	{
+		int y;
+	}
+	int y;
+}
